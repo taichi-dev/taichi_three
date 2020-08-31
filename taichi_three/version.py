cc408d7a2cc9a95276d55b4e18bccaa6c0071d4d
archibate <1931127624@qq.com>
2020-08-31 14:58:03 +0800

    add camera ctrl hint

--- taichi_three/version.py
+++ taichi_three/version.py
@@ -4,3 +4,4 @@ taichi_version = (0, 6, 28)
 
 print(f'[Tai3D] version {".".join(map(str, version))}')
 print(f'[Tai3D] Inputs are welcomed at https://github.com/taichi-dev/taichi_three')
+print(f'[Tai3D] Camera control hints: LMB to orbit, MMB to move, RMB to scale')
