d41ab31fc56b8ff89cc41b45775b15f97b5a1445
archibate <1931127624@qq.com>
2020-10-11 17:03:59 +0800

    enhance SimpleModel

--- docs/_media/hello_triangle.py
+++ docs/_media/hello_triangle.py
@@ -4,17 +4,17 @@ scene = t3.Scene()
 camera = t3.Camera()
 scene.add_camera(camera)
 
-model = t3.SimpleModel(faces_n=2, pos_n=3)
+model = t3.SimpleModel()
 scene.add_model(model)
 
-model.pos[0] = [+0.0, +0.5, 0.0]
-model.pos[1] = [+0.5, -0.5, 0.0]
-model.pos[2] = [-0.5, -0.5, 0.0]
-model.clr[0] = [1.0, 0.0, 0.0]
-model.clr[1] = [0.0, 1.0, 0.0]
-model.clr[2] = [0.0, 0.0, 1.0]
-model.faces[0] = [0, 1, 2]
-model.faces[1] = [0, 2, 1]
+model.gl.Begin('GL_TRIANGLES')
+model.gl.Color(1.0, 0.0, 0.0)
+model.gl.Vertex(+0.0, +0.5, 0.0)
+model.gl.Color(0.0, 1.0, 0.0)
+model.gl.Vertex(+0.5, -0.5, 0.0)
+model.gl.Color(0.0, 0.0, 1.0)
+model.gl.Vertex(-0.5, -0.5, 0.0)
+model.gl.End()
 
 gui = t3.GUI('Hello Triangle')
 while gui.running:
