b5df56ee22f525ff4121ee73a42378a6e6ff3f38
archibate <1931127624@qq.com>
2020-12-20 18:18:37 +0800

    bump tina to 0.0.1

--- tina/__init__.py
+++ tina/__init__.py
@@ -2,7 +2,7 @@ bl_info = {
         'name': 'Tina',
         'description': 'A real-time soft renderer based on Taichi programming language',
         'author': '彭于斌 <1931127624@qq.com>',
-        'version': (0, 0, 0),
+        'version': (0, 0, 1),
         'blender': (2, 81, 0),
         'location': 'Render -> Tina',
         'support': 'COMMUNITY',
