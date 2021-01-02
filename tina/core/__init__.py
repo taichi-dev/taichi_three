d7f736394f13443ec11a743a55c85a8327ac9790
archibate <1931127624@qq.com>
2021-01-02 16:57:05 +0800

    lazy import scheme

--- tina/core/__init__.py
+++ tina/core/__init__.py
@@ -1,11 +1,8 @@
-print('[Tina] Loading rasterizer module...')
-
-from .engine import *
-from .triangle import *
-from .particle import *
-from .wireframe import *
-from .volume import *
-from .shader import *
-from .lighting import *
-
-print('[Tina] Loading rasterizer module done')
+if __import__('lazyimp').mock(globals()):
+    from .engine import *
+    from .triangle import *
+    from .particle import *
+    from .wireframe import *
+    from .volume import *
+    from .shader import *
+    from .lighting import *
