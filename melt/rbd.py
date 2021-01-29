572ddaa51f3817e2ecf2aaec638efaee93165ba7
archibate <1931127624@qq.com>
2021-01-29 15:29:45 +0800

    settle rbd mass spring system

--- melt/rbd.py
+++ melt/rbd.py
@@ -1,47 +1,64 @@
+# https://developer.nvidia.com/gpugems/gpugems3/part-v-physics-simulation/chapter-29-real-time-rigid-body-simulation-gpus
+
 from tina.advans import *
+#from voxelizer import MeshVoxelizer
 
 N = 128
-pos = ti.Vector.field(3, float, N, needs_grad=True)
-U = ti.field(float, (), needs_grad=True)
+R = 0.1
+Ks = 600
+Kd = 6
+Kt = 0
+Dt = 0.001
+Grav = 2.5
+pos = ti.Vector.field(3, float, N)
+vel = ti.Vector.field(3, float, N)
 
-@ti.materialize_callback
 @ti.kernel
 def reset():
     for i in pos:
-        pos[i] = V(ti.random(), ti.random())
-
-@ti.pyfunc
-def sdPlane(a, b, p):
-    n = (b - a).Yx
-    s = (p - a).dot(n)
-    return s / n.norm()
-
-@ti.pyfunc
-def sdCube(a, s, p):
-    s1 = sdPlane(a + V(-s, -s), a + V(-s, +s), p)
-    s2 = sdPlane(a + V(-s, +s), a + V(+s, +s), p)
-    s3 = sdPlane(a + V(+s, +s), a + V(+s, -s), p)
-    s4 = sdPlane(a + V(+s, -s), a + V(-s, -s), p)
-    return max(s1, s2, s3, s4)
+        pos[i] = V(ti.random(), ti.random(), ti.random()) * 2 - 1
+        vel[i] = 0
 
 @ti.kernel
-def calcEnergy():
-    for i in range(N):
-        for j in range(i + 1, N):
-            dis = (pos[i] - pos[j]).norm()
-            U[None] += max(0, 0.1 - dis)
+def substep():
+    for i in pos:
+        acc = V(0., 0., 0.)
+        for j in range(N):
+            if i == j:
+                continue
+            r = pos[j] - pos[i]
+            v = vel[j] - vel[i]
+            rn = r.norm()
+            if rn > R * 2:
+                continue
+            rnn = r / rn
+            fs = -Ks * (R * 2 - rn) * rnn
+            vd = v.dot(rnn) * rnn
+            ft = Kt * (v - vd)
+            fd = Kd * vd
+            acc += fs + fd + ft
+        acc += V(0., -Grav, 0.)
+        vel[i] += acc * Dt
+    for i in pos:
+        cond = (pos[i] < -1 and vel[i] < 0) or (pos[i] > 1 and vel[i] > 0)
+        vel[i] = -0.1 * vel[i] if cond else vel[i]
+        pos[i] += vel[i] * Dt
 
-@ti.kernel
-def stepForward():
-    for i in range(N):
-        pos[i] -= pos.grad[i] * 0.001
+def step():
+    for i in range(32):
+        substep()
 
+scene = tina.Scene()
+model = tina.SimpleParticles(radius=R)
+scene.add_object(model)
 
+reset()
 gui = ti.GUI()
-while gui.running and not gui.get_event(gui.ESCAPE):
-    with ti.Tape(U):
-        calcEnergy()
-    stepForward()
-    print('U =', U[None])
-    gui.circles(pos.to_numpy(), radius=4)
+while gui.running:
+    scene.input(gui)
+    if not gui.is_pressed(gui.SPACE): step()
+    if gui.is_pressed('r'): reset()
+    model.set_particles(pos.to_numpy())
+    scene.render()
+    gui.set_image(scene.img)
     gui.show()
