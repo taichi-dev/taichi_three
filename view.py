38f5003dc27a1ea8070469983e8647a423a798d4
archibate <1931127624@qq.com>
2021-03-21 15:50:12 +0800

    add d2 option

--- view.py
+++ view.py
@@ -6,7 +6,8 @@ import os
 cmap = cm.get_cmap('magma')
 
 
-res = 256, 64, 64
+#res = 256, 64, 64
+res = 512, 128, 1
 rho = ti.field(float, res)
 vel = ti.Vector.field(3, float, res)
 img = ti.field(float, (res[0], res[1]))
@@ -32,7 +33,7 @@ def render():
     for x, y in img:
         ret = 0.0
         cnt = 0
-        for z in range(res[2] // 4, res[2] * 3 // 4):
+        for z in range(res[2] // 4, max(1, res[2] * 3 // 4)):
             ret += color(x, y, z)
             cnt += 1
         img[x, y] = ret / cnt
